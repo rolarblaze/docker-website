2ac1c8b15e4a281be7e0d10728cf94b90ebd801f
rolarblaze <rolarblaze@github.com>
2023-03-04 10:14:18 +0100

    adding Dockerfile to my commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const button  = document.getElementById('btn');
+
+button.addEventListener("onmouseover", function () {
+  //alert("You aare clicking me")
+  document.write("Welcome to my world");
+});
+
+
+
